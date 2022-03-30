5ed8b8e128440fc0e012b14fee2654287d7376b6
martynocchio <martynocchio@mail.ru>
2022-03-30 22:15:31 +0300

    created structure

---
+++ go.mod
@@ -0,0 +1,11 @@
+module telegram-bot
+
+go 1.18
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/kropp/intellij-makefile v0.0.0-20210202093441-7b40402eb003 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	github.com/zhashkevych/go-pocket-sdk v0.0.0-20201222091903-35b3b269a1e8 // indirect
+)
---
+++ pkg/telegram/handlers.go
@@ -0,0 +1,48 @@
+package telegram
+
+import (
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
+
+const (
+	commandStart = "start"
+
+	replyStartTemplate = "access denied, please, follow the link:\n%s"
+)
+
+func (b *Bot) handleCommand(message *tgbotapi.Message) error {
+	switch message.Command() {
+	case commandStart:
+		return b.handleStartCommand(message)
+	default:
+		return b.handleUnknownCommand(message)
+	}
+}
+
+func (b *Bot) handleMessage(message *tgbotapi.Message) {
+	log.Printf("[%s] %s", message.From.UserName, message.Text)
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, message.Text)
+	b.bot.Send(msg)
+}
+
+func (b *Bot) handleStartCommand(message *tgbotapi.Message) error {
+	authLink, err := b.generateAuthorizationLink(message.Chat.ID)
+	if err != nil {
+		return err
+	}
+
+	msg := tgbotapi.NewMessage(message.Chat.ID,
+		fmt.Sprintf(replyStartTemplate, authLink))
+
+	_, err = b.bot.Send(msg)
+	return err
+}
+
+func (b *Bot) handleUnknownCommand(message *tgbotapi.Message) error {
+	msg := tgbotapi.NewMessage(message.Chat.ID, "i don't know this command")
+	_, err := b.bot.Send(msg)
+	return err
+}
